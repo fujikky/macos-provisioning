d6bf322044b90f629f46ee900db19d7906593587
Yusuke Fujiki <yusuke@fujikkys.com>
2016-09-23 14:58:42 +0900

    update install

--- install.sh
+++ install.sh
@@ -5,21 +5,14 @@ TMP_DIR="/tmp/macos-provisioning-master"
 
 echo "Start install & setup scripts."
 
-if [ "$XCODE_INSTALL_PASSWORD" == "" ]; then
-  echo "READ XCODE_INSTALL_PASSWORD : "
-  read -s XCODE_INSTALL_PASSWORD
-fi
-
-echo "SHOW XCODE_INSTALL_PASSWORD : ${XCODE_INSTALL_PASSWORD}"
-
-# cd $DIR_PATH && ${DIR_PATH}/scripts/create_dotfiles.sh
-# cd $DIR_PATH && ${DIR_PATH}/scripts/install_brew.sh
-# cd $DIR_PATH && ${DIR_PATH}/scripts/install_brew_cask.sh
-# cd $DIR_PATH && ${DIR_PATH}/scripts/install_mas.sh
-# cd $DIR_PATH && ${DIR_PATH}/scripts/setup_vim.sh
-# cd $DIR_PATH && ${DIR_PATH}/scripts/setup_ruby.sh
-# cd $DIR_PATH && ${DIR_PATH}/scripts/setup_node.sh
-# cd $DIR_PATH && ${DIR_PATH}/scripts/install_xcode.sh
+cd $DIR_PATH && ${DIR_PATH}/scripts/create_dotfiles.sh
+cd $DIR_PATH && ${DIR_PATH}/scripts/install_brew.sh
+cd $DIR_PATH && ${DIR_PATH}/scripts/install_brew_cask.sh
+cd $DIR_PATH && ${DIR_PATH}/scripts/install_mas.sh
+cd $DIR_PATH && ${DIR_PATH}/scripts/setup_vim.sh
+cd $DIR_PATH && ${DIR_PATH}/scripts/setup_ruby.sh
+cd $DIR_PATH && ${DIR_PATH}/scripts/setup_node.sh
+cd $DIR_PATH && ${DIR_PATH}/scripts/install_xcode.sh
 
 echo "All install & setup scripts done!"
 
