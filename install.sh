fb5aec2319f0c73092fe59bd0bc39919c2059ac6
Yusuke Fujiki <yusuke@fujikkys.com>
2016-09-23 15:00:28 +0900

    fix path

--- install.sh
+++ install.sh
@@ -5,14 +5,14 @@ TMP_DIR="/tmp/macos-provisioning-master"
 
 echo "Start install & setup scripts."
 
-cd $DIR_PATH && ${DIR_PATH}/scripts/create_dotfiles.sh
-cd $DIR_PATH && ${DIR_PATH}/scripts/install_brew.sh
-cd $DIR_PATH && ${DIR_PATH}/scripts/install_brew_cask.sh
-cd $DIR_PATH && ${DIR_PATH}/scripts/install_mas.sh
-cd $DIR_PATH && ${DIR_PATH}/scripts/setup_vim.sh
-cd $DIR_PATH && ${DIR_PATH}/scripts/setup_ruby.sh
-cd $DIR_PATH && ${DIR_PATH}/scripts/setup_node.sh
-cd $DIR_PATH && ${DIR_PATH}/scripts/install_xcode.sh
+cd $TMP_DIR && ${TMP_DIR}/scripts/create_dotfiles.sh
+cd $TMP_DIR && ${TMP_DIR}/scripts/install_brew.sh
+cd $TMP_DIR && ${TMP_DIR}/scripts/install_brew_cask.sh
+cd $TMP_DIR && ${TMP_DIR}/scripts/install_mas.sh
+cd $TMP_DIR && ${TMP_DIR}/scripts/setup_vim.sh
+cd $TMP_DIR && ${TMP_DIR}/scripts/setup_ruby.sh
+cd $TMP_DIR && ${TMP_DIR}/scripts/setup_node.sh
+cd $TMP_DIR && ${TMP_DIR}/scripts/install_xcode.sh
 
 echo "All install & setup scripts done!"
 
