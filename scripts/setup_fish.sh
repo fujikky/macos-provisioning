c1486f560a2837c6208aa2001c651d2817c89103
Yusuke Fujiki <yusuke@fujikkys.com>
2019-12-20 16:12:22 +0900

    Fix setup script

--- scripts/setup_fish.sh
+++ scripts/setup_fish.sh
@@ -1,9 +1,9 @@
 #!/usr/local/bin/fish
 
-if test -e ~/.config/fish/functions/fisher.fish
-else
-  echo "Install fisherman"
-  curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs git.io/fisherman
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
 end
 
 fisher
