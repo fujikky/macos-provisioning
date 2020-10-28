798dc339fde24da91d8e75ebaea0b5d60e2fc574
Yusuke Fujiki <yusuke@fujikkys.com>
2020-10-28 12:06:28 +0900

    fix anyenv init script

--- dotfiles/.config/fish/conf.d/anyenv.fish
+++ dotfiles/.config/fish/conf.d/anyenv.fish
@@ -1,5 +1,4 @@
 # anyenv
 if test -x $HOME/.anyenv
-  set -g fish_user_paths "$HOME/.anyenv/bin" $fish_user_paths
-  status --is-interactive; and source (anyenv init -|psub)
+  anyenv init - fish | source
 end
