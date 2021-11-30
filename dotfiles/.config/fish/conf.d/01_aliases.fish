e703ff1ce7eb13742efdf146bc2cb6647873d170
Yusuke Fujiki <yusuke@fujikkys.com>
2021-11-30 17:53:49 +0900

    :sparkles: add alias to launch simulators

--- dotfiles/.config/fish/conf.d/01_aliases.fish
+++ dotfiles/.config/fish/conf.d/01_aliases.fish
@@ -1,13 +1,11 @@
 # aliases
 
 alias g="git"
-
 alias y="yarn"
-
 alias b="bundle"
 
 alias ll="ls -lha"
-
-alias simulator="open (xcode-select -p)/Applications/Simulator.app"
-
 alias ip="ifconfig -a | grep inet[^6] | sed 's/.*inet[^6][^0-9]*\([0-9.]*\)[^0-9]*.*/\1/' | grep -v '^127\.'"
+
+alias ios-simulator="open (xcode-select -p)/Applications/Simulator.app"
+alias android-simulator="~/Library/Android/sdk/emulator/emulator -avd (~/Library/Android/sdk/emulator/emulator -list-avds | head)"
