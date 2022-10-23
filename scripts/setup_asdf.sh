3f247200689057ccf0f0d8ba9ad51b26a7a827c1
Yusuke Fujiki <yusuke@fujikkys.com>
2022-10-23 23:00:49 +0900

    :pushpin: use pinned nodejs version

--- scripts/setup_asdf.sh
+++ scripts/setup_asdf.sh
@@ -12,11 +12,12 @@ asdf install ruby $RUBY_VERSION
 asdf global ruby $RUBY_VERSION
 
 # Install Node
+NODE_VERSION=$(curl -s https://nodejs.org/dist/index.json | jq -r '.[] | select(.lts != false) | .version' | head -1 | sed 's/^v//')
 set +e
 asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 set -e
-asdf install nodejs lts
-asdf global nodejs lts
+asdf install nodejs $NODE_VERSION
+asdf global nodejs $NODE_VERSION
 npm i -g yarn gitmoji-cli commitizen
 
 # Install Go
