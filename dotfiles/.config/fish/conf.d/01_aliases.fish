123a7916a51cebb682178e10071207d6e882aee6
Yusuke Fujiki <yusuke@fujikkys.com>
2024-12-27 13:29:45 +0900

    Fix alias

--- dotfiles/.config/fish/conf.d/01_aliases.fish
+++ dotfiles/.config/fish/conf.d/01_aliases.fish
@@ -9,7 +9,7 @@ alias ll="ls -lha"
 alias ip="ifconfig -a | grep inet[^6] | sed 's/.*inet[^6][^0-9]*\([0-9.]*\)[^0-9]*.*/\1/' | grep -v '^127\.'"
 
 alias ios-simulator="open (xcode-select -p)/Applications/Simulator.app"
-alias android-simulator="~/Library/Android/sdk/emulator/emulator -avd (~/Library/Android/sdk/emulator/emulator -list-avds | head)"
+alias android-simulator="~/Library/Android/sdk/emulator/emulator -avd (~/Library/Android/sdk/emulator/emulator -list-avds | head -n 1)"
 
 alias gc="git-clean-branches"
-alias dc="docker-compose"
+alias dc="docker compose"
